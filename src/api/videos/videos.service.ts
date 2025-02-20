93fa3fea2cd24581cf9ef43ba0fccb9d42554602
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-02-20 08:19:18 -0300

    update correção de função

--- src/api/videos/videos.service.ts
+++ src/api/videos/videos.service.ts
@@ -1,17 +1,18 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { ErrorEntity } from 'src/entities/error.entity';
 import * as fs from 'fs';
+import { Video } from './entities/video.entity';
 
 @Injectable()
 export class VideosService {
-  findAll() {
+  findAll(): Promise<Video[] | ErrorEntity> {
     try {
       // listar todos os videos que estão na pasta /var/www/html/ar_interface/assets/notanfe/
       const lista = fs
         .readdirSync('/var/www/html/ar_interface/assets/notanfe')
         .filter((file) => file.endsWith('.mp4'));
 
-      const retorno = lista.map((i: any, index: number) => {
+      const retorno: Video[] = lista.map((i: any, index: number) => {
         //pegar o i e retirar o .mp4 e depois substituir o _ por espaco
         const tituloNome = i.replace('.mp4', '').replace(/_/g, ' ');
         return {
@@ -20,7 +21,13 @@ export class VideosService {
           url: `https://arinterface.com.br/assets/notanfe/${i}`,
         };
       });
-      return retorno.length > 0 ? retorno : [];
+      if (!retorno || retorno.length === 0) {
+        const retorno: ErrorEntity = {
+          message: 'Nenhum video encontrado',
+        };
+        throw new HttpException(retorno, 400);
+      }
+      return Promise.resolve(retorno);
     } catch (error) {
       const retorno: ErrorEntity = {
         message: 'Falha ao criar o usuário',
